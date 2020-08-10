0b79e3d4022ac2f66dd33e60caab9b242287348b
James Monks <James.Monks@academytrainee.com>
2020-08-10 17:53:27 +0100

    skill descriptions load

--- htmlpages/buyskill.js
+++ htmlpages/buyskill.js
@@ -1,156 +1,78 @@
 document.querySelector("#armourdrop").addEventListener("click", function(a) {
     a.preventDefault()
+    //loads tree picture
     document.getElementById("treehead").innerHTML="Armour Tree"
     document.getElementById("picspace").innerHTML="<img src='armourtree.png' width=100%>"
-    fetch(`http://localhost:8010/skills/listbytree/1`)
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-          document.getElementById("skillform").innerHTML=""
-          document.getElementById("skillform").innerHTML+=`<label for="chooseskill">Choose a skill:</label>
-          <select class="form-control" id="chooseskill">
-          </select>`
-          // Examine the text in the response
-          response.json().then(function(data) {
-            console.log(data);
-            for (let i =0; i<data.length;i++) {
-
-              let formoption = `<option>${data[i].skill_name}</option>`
-            
-            document.getElementById("chooseskill").innerHTML+=formoption;
-            }
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+    //retrieve list of skills from the database
+    getSkills(1)
     })
 
 document.querySelector("#bodydrop").addEventListener("click", function(a) {
     a.preventDefault()
     document.getElementById("treehead").innerHTML="Body Tree"
     document.getElementById("picspace").innerHTML="<img src='bodytree.png' width=100%>"
-    fetch(`http://localhost:8010/skills/listbytree/2`)
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-          document.getElementById("skillform").innerHTML=""
-          document.getElementById("skillform").innerHTML+=`<label for="chooseskill">Choose a skill:</label>
-          <select class="form-control" id="chooseskill">
-          </select>`
-          // Examine the text in the response
-          response.json().then(function(data) {
-            console.log(data);
-            for (let i =0; i<data.length;i++) {
-
-              let formoption = `<option>${data[i].skill_name}</option>`
-            
-            document.getElementById("chooseskill").innerHTML+=formoption;
-            }
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+    getSkills(2)
 })
 document.querySelector("#resdrop").addEventListener("click", function(a) {
     a.preventDefault()
     document.getElementById("treehead").innerHTML="Resistance Tree"
     document.getElementById("picspace").innerHTML="<img src='resistancetree.png' width=100%>"
-    fetch(`http://localhost:8010/skills/listbytree/3`)
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-          document.getElementById("skillform").innerHTML=""
-          document.getElementById("skillform").innerHTML+=`<label for="chooseskill">Choose a skill:</label>
-          <select class="form-control" id="chooseskill">
-          </select>`
-          // Examine the text in the response
-          response.json().then(function(data) {
-            console.log(data);
-            for (let i =0; i<data.length;i++) {
-
-              let formoption = `<option>${data[i].skill_name}</option>`
-            
-            document.getElementById("chooseskill").innerHTML+=formoption;
-            }
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+    getSkills(3)
 })
 document.querySelector("#magicdrop").addEventListener("click", function(a) {
     a.preventDefault()
     document.getElementById("treehead").innerHTML="Magic Tree"
     document.getElementById("picspace").innerHTML="<img src='magictree.png' width=100%>"
-    fetch(`http://localhost:8010/skills/listbytree/4`)
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-          document.getElementById("skillform").innerHTML=""
-          document.getElementById("skillform").innerHTML+=`<label for="chooseskill">Choose a skill:</label>
-          <select class="form-control" id="chooseskill">
-          </select>`
-          // Examine the text in the response
-          response.json().then(function(data) {
-            console.log(data);
-            for (let i =0; i<data.length;i++) {
-
-              let formoption = `<option>${data[i].skill_name}</option>`
-            
-            document.getElementById("chooseskill").innerHTML+=formoption;
-            }
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+    getSkills(4)
 })
 document.querySelector("#weapondrop").addEventListener("click", function(a) {
     a.preventDefault()
     document.getElementById("treehead").innerHTML="Weapon Tree"
     document.getElementById("picspace").innerHTML="<img src='weapontree.png' width=100%>"
-    fetch(`http://localhost:8010/skills/listbytree/5`)
+    getSkills(5)
+})
+function getSkills(tree_id) {
+fetch(`http://localhost:8010/skills/listbytree/${tree_id}`)
     .then(function(response) {
         if (response.status !== 200) {
             console.log('Looks like there was a problem. Status Code: ' +
               response.status);
             return;
           }
-          document.getElementById("skillform").innerHTML=""
-          document.getElementById("skillform").innerHTML+=`<label for="chooseskill">Choose a skill:</label>
-          <select class="form-control" id="chooseskill">
-          </select>`
+          document.getElementById("skilldrophead").innerHTML="Choose A Skill"
+          document.getElementById("skillform").innerHTML=`<button id="chooseskillbutt" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" width="50%">
+          Skills
+          </button>
+          <div id ="chooseskill" class="dropdown-menu" aria-labelledby="dropdownMenu2"></div>`
           // Examine the text in the response
           response.json().then(function(data) {
             console.log(data);
             for (let i =0; i<data.length;i++) {
 
-              let formoption = `<option>${data[i].skill_name}</option>`
-            
-            document.getElementById("chooseskill").innerHTML+=formoption;
-            }
+              let formoption = `<button id="skillget_${data[i].skill_id}" onclick="loadSkill(${data[i].skill_id})" class="dropdown-item" type="button">${data[i].skill_name}</button>`
+              document.getElementById("chooseskill").innerHTML+=formoption;
+            }            
           });
         }
       )
       .catch(function(err) {
         console.log('Fetch Error :-S', err);
       });
-})
+}
+function loadSkill(n) {
+  fetch(`http://localhost:8010/skills/findbyid/${n}`)
+    .then(function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+          response.json().then(function(data) {
+            
+            document.getElementById("skillnamehead").innerHTML="Skill Name:"
+            document.getElementById("skillnamepara").innerHTML= data.skill_name
+            document.getElementById("descripheading").innerHTML="Skill Description:"
+            document.getElementById("skillbuydescription").innerHTML=data.description
+
+          })  
+})}
