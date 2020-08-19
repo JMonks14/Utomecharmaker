ef9566ee87b7faef9e510c19228386712565d5d3
James Monks <James.Monks@academytrainee.com>
2020-08-19 18:57:05 +0100

    fixed testing issue

--- TomeBackendApp/src/main/java/com/tome/main/Controllers/CharSkillController.java
+++ TomeBackendApp/src/main/java/com/tome/main/Controllers/CharSkillController.java
@@ -37,9 +37,9 @@ public class CharSkillController {
 	}
 	
 	@DeleteMapping("reset/{id}")
-	public void reset(@PathVariable int id) {
-		  this.service.reset(id);
-		
+	public ResponseEntity<Integer> reset(@PathVariable int id) {
+		this.service.reset(id);
+		return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
 	}
 
 }
--- TomeBackendApp/src/test/java/com/tome/main/ControllerTests/CharSkillControllerTests.java
+++ TomeBackendApp/src/test/java/com/tome/main/ControllerTests/CharSkillControllerTests.java
@@ -3,6 +3,7 @@ package com.tome.main.ControllerTests;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,29 +21,28 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tome.main.Enitities.CharSpell;
+import com.tome.main.Enitities.CharSkills;
 import com.tome.main.Enitities.Characters;
 import com.tome.main.Enitities.Player;
-import com.tome.main.Enitities.Spell;
-import com.tome.main.Repos.CharSpellRepo;
+import com.tome.main.Enitities.Skill;
+import com.tome.main.Repos.CharSkillRepo;
 import com.tome.main.Repos.CharacterRepo;
 import com.tome.main.Repos.PlayerRepo;
-import com.tome.main.Repos.SpellRepo;
-
+import com.tome.main.Repos.SkillRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CharSpellControllerTest {
+public class CharSkillControllerTests {
 	
 	@Autowired
 	private MockMvc mock;
 	
 	@Autowired
-	CharSpellRepo CSrepo;
+	CharSkillRepo CSrepo;
 	
 	@Autowired
-	private SpellRepo repo;
+	private SkillRepo repo;
 	
 	@Autowired
 	private PlayerRepo Prepo;
@@ -53,95 +53,101 @@ public class CharSpellControllerTest {
 	@Autowired
 	private ObjectMapper mapper = new ObjectMapper();
 	
-	Spell spell1 = new Spell("blast",0,1,"ranged","blasts a guy");
-	Spell spell2 = new Spell("bind",0,1,"ranged","binds a guy");
-	Spell spell3 = new Spell("fling",0,1,"fling","flings a guy");
-	Spell spell4 = new Spell("mass fling",0,3,"mass","flings lots of guys");
-	List<Spell> spells = new ArrayList<>();
 	private Player testplayer = new Player("Bob", "Smith", "Smithy", "neilthebaby",0);
 	private Characters testChar = new Characters("Steve","human","Ardglass","boring",1,true,3,3,0,0,0,0,0);
 	int Pid;
 	int Cid;
 	
-	CharSpell charspell2 = new CharSpell(1,0,0);
-	CharSpell charspell3 = new CharSpell(2,0,0);
-	CharSpell charspell4 = new CharSpell(3,0,0);
-	CharSpell charspell1 = new CharSpell(4,0,0);
-	List<CharSpell> charspells = new ArrayList<CharSpell>();
+	Skill skill1 = new Skill(1,"skill1","a skill",1,0,0,0,0,0,false);
+	Skill skill2 = new Skill(2,"skill2","another skill",1,0,0,0,0,0,false);
+	Skill skill3 = new Skill(3,"skill3","a third skill",1,0,0,0,0,0,false);
+	Skill skill4 = new Skill(4,"skill4","a 4th skill",1,0,0,0,0,0,true);
+	List<Skill> skills = new ArrayList<>();
+	
+	CharSkills charskill1 = new CharSkills(4,1,1);
+	CharSkills charskill2 = new CharSkills(1,1,1);
+	CharSkills charskill3 = new CharSkills(2,1,2);
+	CharSkills charskill4 = new CharSkills(3,1,3);
+	List<CharSkills> charskills = new ArrayList<CharSkills>();
 	
 	@Before
-	public void init () {
+	public void init() {
 		CSrepo.deleteAll();
 		Crepo.deleteAll();
 		Prepo.deleteAll();
-		spells=repo.listAll();
-		repo.deleteInBatch(spells);
-		spell1=repo.save(spell1);
-		spell2=repo.save(spell2);
-		spell3=repo.save(spell3);
-		spell4=repo.save(spell4);
-		spells.clear();
-		spells.add(spell1);
-		spells.add(spell2);
-		spells.add(spell3);
-		spells.add(spell4);
+		skills=repo.findByTreeId(1);
+		repo.deleteAll(skills);
 		testplayer=Prepo.save(testplayer);
 		Pid=testplayer.getPlayer_id();
 		testChar.setFk_player_id(Pid);
 		testChar=Crepo.save(testChar);
 		Cid=testChar.getChar_id();
-		charspell1.setFk_char_id(Cid);
-		charspell1.setFk_spell_id(spell1.getSpell_id());
-		charspell1=CSrepo.save(charspell1);
-		charspell2.setFk_char_id(Cid);
-		charspell2.setFk_spell_id(spell2.getSpell_id());
-		charspell2=CSrepo.save(charspell2);
-		charspell3.setFk_char_id(Cid);
-		charspell3.setFk_spell_id(spell3.getSpell_id());
-		charspell3=CSrepo.save(charspell3);
-		charspell4.setFk_char_id(Cid);
-		charspell4.setFk_spell_id(spell4.getSpell_id());
-		charspell4=CSrepo.save(charspell4);		
+		skill1=repo.save(skill1);
+		skill2=repo.save(skill2);
+		skill3=repo.save(skill3);
+		skill4=repo.save(skill4);
+		skills.clear();
+		skills.add(skill1);
+		skills.add(skill2);
+		skills.add(skill3);
+		skills.add(skill4);
+		charskill1.setFk_char_id(Cid);
+		charskill1.setFk_skill_id(skill1.getSkill_id());
+		charskill1=CSrepo.save(charskill1);
+		charskill2.setFk_char_id(Cid);
+		charskill2.setFk_skill_id(skill2.getSkill_id());
+		charskill2=CSrepo.save(charskill2);
+		charskill3.setFk_char_id(Cid);
+		charskill3.setFk_skill_id(skill3.getSkill_id());
+		charskill3=CSrepo.save(charskill3);
+		charskill4.setFk_char_id(Cid);
+		charskill4.setFk_skill_id(skill4.getSkill_id());
+		charskill4=CSrepo.save(charskill4);	
 	}
 	@Test
 	public void testBuy() throws JsonProcessingException, Exception {
 		MockHttpServletRequestBuilder mockRequest = 
-				MockMvcRequestBuilders.request(HttpMethod.POST, "/charspell/buy");
+				MockMvcRequestBuilders.request(HttpMethod.POST, "/charskills/buy");
 		
 		mockRequest.contentType(MediaType.APPLICATION_JSON)
-			.content(this.mapper.writeValueAsString(charspell1))
+			.content(this.mapper.writeValueAsString(charskill1))
 			.accept(MediaType.APPLICATION_JSON);
 	
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();
-		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(charspell1));
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(charskill1));
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
 	}
 	
 	@Test
 	public void testCharSpellIds() throws JsonProcessingException, Exception {
-		charspells.add(charspell1);
-		charspells.add(charspell2);
-		charspells.add(charspell3);
-		charspells.add(charspell4);
+		charskills.add(charskill1);
+		charskills.add(charskill2);
+		charskills.add(charskill3);
+		charskills.add(charskill4);
 		MockHttpServletRequestBuilder mockRequest = 
-				MockMvcRequestBuilders.request(HttpMethod.GET, "/charspell/byid/" + Cid);
+				MockMvcRequestBuilders.request(HttpMethod.GET, "/charskills/byid/" + Cid);
 		
 		mockRequest.contentType(MediaType.APPLICATION_JSON)
-			.content(this.mapper.writeValueAsString(charspells))
+			.content(this.mapper.writeValueAsString(charskills))
 			.accept(MediaType.APPLICATION_JSON);
 	
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();
-		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(charspells));
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(charskills));
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
 	}
 	
 	@Test
 	public void testReset() throws JsonProcessingException, Exception {
 		MockHttpServletRequestBuilder mockRequest = 
-				MockMvcRequestBuilders.request(HttpMethod.DELETE, "/charspell/reset/" + Cid);
+				MockMvcRequestBuilders.request(HttpMethod.DELETE, "/charskills/reset/" + Cid);
 	
 		ResultMatcher matchStatus = MockMvcResultMatchers.status().isNoContent();
 		this.mock.perform(mockRequest).andExpect(matchStatus);
 	}
 	
+	@After
+	public void erase() {
+		CSrepo.deleteAll();
+	}
+
 }
